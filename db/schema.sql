ba3fa3d7f6e2857a04e481738f5aa69f1a48170a
Carl Robinson Jr <CarlRobinsonJr@gmail.com>
2019-10-17 10:52:16 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,2 @@
 CREATE DATABASE games_db;
 USE games_db;
-CREATE TABLE games
-(
-	rank INT NOT NULL,
-	name varchar(255) NOT NULL,	
-	platform varchar(255) NOT NULL,	
-	year YEAR NULL,
-	genre varchar(255) NOT NULL,
-	publisher varchar(255) NOT NULL,
-	na_Sales DECIMAL(10,4) NULL,
-	eu_Sales DECIMAL(10,4) NULL,
-	jp_Sales DECIMAL(10,4) NULL,
-	other_Sales DECIMAL(10,4) NULL,
-	global_Sales DECIMAL(10,4) NULL,
-	PRIMARY KEY (rank)
-);
